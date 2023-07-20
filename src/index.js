bba3b4737e9941a9f5603e36e5e04fdc9919f00f
EFTEKHER <eftekherali2000@gmail.com>
2023-07-21 02:42:48 +0600

    Day1-start-with-zero

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React App !</h1>;
+}
+//React 18
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> // it will render twice and find help to get bug
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+//React  before 18
+React.Render(<App />    )
+
+*/
