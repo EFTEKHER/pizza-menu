fbde1e346a17c176d9420977c9418249a03558fa
EFTEKHER <eftekherali2000@gmail.com>
2023-07-22 14:15:31 +0600

    done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React App !</h1>;
+  return <div>
+  <h1>Hello React App !!!!</h1>
+  <Pizza/>
+  <Pizza/>
+  <Pizza/>
+  </div>;
+}
+
+function Pizza()
+{
+  return <div>
+  <img src="pizzas/spinaci.jpg" alt="Pizza Spinacci" />
+  <h2>Pizza Spinacci</h2>
+  <p>Tomato ,mozarella,spinach and ricottea cheese</p>
+  </div>
 }
 //React 18
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode> // it will render twice and find help to get bug
+    // it will render twice and find help to get bug
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
